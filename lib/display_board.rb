b3718296bc6c9c8821c92b15c34585d4cba94fdc
Julia J <juliajen.prog@gmail.com>
2017-09-22 14:58:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 #Define method for display_board and test in IRB
 
 def display_board
-  puts = " #{"   "} | #{"   "} | #{"   "} "
+  puts = '#{"   "} | #{"   "} | #{"   "}'
   puts = "-----------"
-  puts = " #{"   "} | #{"   "} | #{"   "} "
+  puts = '#{"   "} | #{"   "} | #{"   "}'
   puts = "-----------"
-  puts = " #{"   "} | #{"   "} | #{"   "} "
+  puts = '#{"   "} | #{"   "} | #{"   "}'
 end
 display_board
 
