56e286033630907b06ff8c81fc84035d612c4bcb
Julia J <juliajen.prog@gmail.com>
2017-09-22 15:56:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,32 +1,32 @@
 #Define method for display_board and test in IRB
 def display_board
-  puts = "   ",|,"   ",|,"   "
+  puts = "   |   |   "
   puts = "-----------"
-  puts = "   ",|,"   ",|,"   "
+  puts = "   |   |   "
   puts = "-----------"
-  puts = "   ",|,"   ",|,"   "
+  puts = "   |   |   "
 end
 display_board
 
 
-def display_board
-  puts = '"   "|"   "|"   "'
-  puts = "-----------"
-  puts = '"   "|"   "|"   "'
-  puts = "-----------"
-  puts = '"   "|"   "|"   "'
-end
-display_board
-
-
-def display_board
-  puts = '#{"   "} | #{"   "} | #{"   "}'
-  puts = "-----------"
-  puts = '#{"   "} | #{"   "} | #{"   "}'
-  puts = "-----------"
-  puts = '#{"   "} | #{"   "} | #{"   "}'
-end
-display_board
+# def display_board
+#   puts = '"   "|"   "|"   "'
+#   puts = "-----------"
+#   puts = '"   "|"   "|"   "'
+#   puts = "-----------"
+#   puts = '"   "|"   "|"   "'
+# end
+# display_board
+# 
+# 
+# def display_board
+#   puts = '#{"   "} | #{"   "} | #{"   "}'
+#   puts = "-----------"
+#   puts = '#{"   "} | #{"   "} | #{"   "}'
+#   puts = "-----------"
+#   puts = '#{"   "} | #{"   "} | #{"   "}'
+# end
+# display_board
 
 
 # def display_board (board) # Method signature
